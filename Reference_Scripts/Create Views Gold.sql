4617fd9d1612724242b6edadb91efdf3828f4f98
Prasanth Kandikatla <008480414@coyote.csusb.edu>
2025-08-25 00:53:53 -0700

    Gold Views

---
+++ Reference_Scripts/Create Views Gold.sql
@@ -0,0 +1,98 @@
+---------------
+--Calendar
+---------------
+CREATE VIEW serving.Calendar
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Calendar/',
+        FORMAT = 'PARQUET'
+    ) AS Query1
+GO
+
+---------------
+--Customers
+---------------
+CREATE VIEW serving.Customers
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Customers/',
+        FORMAT = 'PARQUET'
+    ) AS Query2
+GO
+   
+
+--------------------
+CREATE VIEW serving.Product_Categories
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Product_Categories/',
+        FORMAT = 'PARQUET'
+    ) AS Query3
+GO
+      
+
+CREATE VIEW serving.Product_Subcategories
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Product_Subcategories/',
+        FORMAT = 'PARQUET'
+    ) AS Query4
+GO
+
+CREATE VIEW serving.Products
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Products/',
+        FORMAT = 'PARQUET'
+    ) AS Query5
+GO
+   
+CREATE VIEW serving.Returns
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Returns/',
+        FORMAT = 'PARQUET'
+    ) AS Query6
+GO
+
+CREATE VIEW serving.Sales
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Sales/',
+        FORMAT = 'PARQUET'
+    ) AS Query7
+GO
+
+CREATE VIEW serving.Territories
+AS
+SELECT
+    *
+FROM 
+    OPENROWSET(
+        BULK 'https://awstoragedatalakekp2499.dfs.core.windows.net/silver/Territories/',
+        FORMAT = 'PARQUET'
+    ) AS Query8
+GO
+
+
